9348f48a86f1b7bbece240fff452afa13e2b1fc0
lixin <singer.lee@qq.com>
2018-03-14 21:00:10 +0800

    0314晚 于千锋

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require("gulp");
+var connect= require('gulp-connect');
+gulp.task("copy-index",function(){
+	return gulp.src("index.html").pipe(gulp.dest("dist")).pipe(connect.reload());
+});
+
+gulp.task("watch",function(){
+	 gulp.watch('index.html',['copy-index']); 
+})
+ 
+gulp.task('sever',function(){ 
+	connect.server({root:'dist',livereload:true});
+})  
+
+gulp.task('default',['sever','watch']); 
